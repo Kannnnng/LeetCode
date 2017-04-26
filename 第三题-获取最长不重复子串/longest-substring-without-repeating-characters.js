65f61e6ad863e6e1f0171814c9851b766535886a
Kannnnng <575664647@qq.com>
2017-04-26 18:10:53 +0800

    修改。

--- 第三题-获取最长不重复子串/longest-substring-without-repeating-characters.js
+++ 第三题-获取最长不重复子串/longest-substring-without-repeating-characters.js
@@ -9,21 +9,34 @@
  * @return {number}
  */
 var myLengthOfLongestSubstring = function(s) {
+  var index = 0;
   var max = 0;
-  var now = 0;
-  var length = s.length;
-  for (var i = 0; i < length; i++) {
-    var temp = s.slice(now, i);
-    if (s.charAt(now) === s.charAt(i) || temp.indexOf(s.charAt(i)) !== -1) {
-      if (max < (i - now)) {
-        max = i - now;
-      }
-      now = i;
+  var str = "";
+  var temp = "";
+  for (var i = 0, length = s.length; i < length; i++) {
+    temp = s.charAt(i);
+    index = str.indexOf(temp);
+    if (index === -1) {
+      str += temp;
+      max = str.length > max ? str.length : max;
+    } else {
+      str = s.substring(index + 1, i + 1);
     }
   }
   return max;
 };
 
-var max = myLengthOfLongestSubstring('au');
+var max = myLengthOfLongestSubstring('aab');
+console.log(max);
 
+var max = myLengthOfLongestSubstring('bbtablud');
+console.log(max);
+
+var max = myLengthOfLongestSubstring('bbbbb');
+console.log(max);
+
+var max = myLengthOfLongestSubstring('pwwkew');
+console.log(max);
+
+var max = myLengthOfLongestSubstring('13546273');
 console.log(max);
