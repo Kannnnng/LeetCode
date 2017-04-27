55a443147cc5e8e1698b9281b835a220003a9f23
Kannnnng <575664647@qq.com>
2017-04-27 13:56:11 +0800

    添加第四题。

---
+++ 第四题-获取最长的回文字符串/longest-palindromic-substring.js
@@ -0,0 +1,18 @@
+/**
+ * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+ *
+ * Input: "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ *
+ * Input: "cbbd"
+ * Output: "bb"
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+  var
+};
