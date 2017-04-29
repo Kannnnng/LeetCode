753b285e09ec28e32d712e39275b2763843f4fac
Kannnnng <575664647@qq.com>
2017-04-29 10:50:49 +0800

    提交代码。
    在 git 忽略文件配置中添加 sublime 项目文件。
    按照一个思路（以每一个字符为中心点向两边扩散寻找最长回文）写了一点第五题，但是运行错误，已知原因是没有分奇数与偶数的情况。

--- 第五题-获取最长的回文字符串/longest-palindromic-substring.js
+++ 第五题-获取最长的回文字符串/longest-palindromic-substring.js
@@ -13,6 +13,27 @@
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function(s) {
-  var
+var myLongestPalindrome = function(s) {
+  if (s.length === 0) {
+    return 0;
+  }
+  var max = 1;
+  for (var i = 0, j = 1, length = s.length; i < length - 1; i++, j = 1) {
+    console.log(i);
+    while (s.charAt(i - j) === s.charAt(i + j)) {
+      console.log(j, max);
+      max = j > max ? j : max;
+      j++;
+    }
+  }
+  return max;
 };
+
+var test = myLongestPalindrome('babad');
+// console.log(test);
+// var test = myLongestPalindrome('cbbd');
+// console.log(test);
+// var test = myLongestPalindrome('a');
+// console.log(test);
+// var test = myLongestPalindrome('afgjkkffsfa');
+// console.log(test);
