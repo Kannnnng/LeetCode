de40421878a2a956d7fe28a28e47a1c19fbc8ce1
Kannnnng <575664647@qq.com>
2017-04-29 11:06:39 +0800

    添加一个大神的写法，有时间了看一看。

--- 第五题-获取最长的回文字符串/longest-palindromic-substring.js
+++ 第五题-获取最长的回文字符串/longest-palindromic-substring.js
@@ -29,11 +29,28 @@ var myLongestPalindrome = function(s) {
   return max;
 };
 
-var test = myLongestPalindrome('babad');
-// console.log(test);
-// var test = myLongestPalindrome('cbbd');
-// console.log(test);
-// var test = myLongestPalindrome('a');
-// console.log(test);
-// var test = myLongestPalindrome('afgjkkffsfa');
-// console.log(test);
+var longestPalindrome = function(s) {
+  var length = s.length;
+  if (length === 0) return "";
+  if (length === 1) return s;
+  var min_start = 0, max_len = 1;
+  for (var i = 0; i < length;) {
+    if (length - i <= max_len / 2) break;
+    var j = i, k = i;
+    while (k < length-1 && s[k+1] == s[k]) ++k; // Skip duplicate characters.
+    i = k+1;
+    while (k < length-1 && j > 0 && s[k + 1] == s[j - 1]) { ++k; --j; } // Expand.
+    var new_len = k - j + 1;
+    if (new_len > max_len) { min_start = j; max_len = new_len; }
+  }
+  return s.substr(min_start, max_len);
+};
+
+var test = longestPalindrome('babad');
+console.log(test);
+var test = longestPalindrome('cbbd');
+console.log(test);
+var test = longestPalindrome('a');
+console.log(test);
+var test = longestPalindrome('afgjkkffsfa');
+console.log(test);
